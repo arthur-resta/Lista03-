5e2859da21a281711e16d37f4063bbcada5b0cc1
Resta <arthur.elena@edu.senai.br>
2026-03-31 13:00:36 -0300

    Add files via upload

---
+++ src/pctEx01/Exercicio01.java
@@ -0,0 +1,42 @@
+package pctEx01;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner receba = new Scanner(System.in);
+		char option;
+		double preco = 0;
+		int quantidade;
+		
+		System.out.println("Menu Padaria");
+		System.out.println("a- café (4$)");
+		System.out.println("b- Suco(6$)");
+		System.out.println("c- Sanduiche (10$)");
+		System.out.println("Escolha a opção");
+		
+		option = receba.next().charAt(0);
+		System.out.println("Digite a quantidade");
+		quantidade = receba.nextInt();
+		
+		switch(option) {
+		case 'a':
+			preco = 4*quantidade;
+			break;
+		case 'b':
+			preco = 6*quantidade;
+			break;
+		case 'c':
+			preco = 10*quantidade;
+			break;
+			default :
+				System.out.println("opçao invalida");
+				
+		}
+		System.out.println("Total da compra R$ " + preco);
+		
+	}
+
+}
